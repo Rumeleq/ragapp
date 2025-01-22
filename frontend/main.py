e73cd9889ca60c031671eae0430d407b8ec9acf6
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-22 19:16:46 +0100

    take the user prompt and store it in a variable

--- frontend/main.py
+++ frontend/main.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("RAG app")
 st.write("Welcome in the conversation with chatbot, that will tell you about tech meetups in Poland!")
+
+user_prompt = st.text_input("You: ", "")
