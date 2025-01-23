0b6a77bf2e8812d282ded453092df537e5f35c05
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-23 19:15:56 +0100

    add asynchronous approach of creating a response

--- frontend/main.py
+++ frontend/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import time
 
@@ -16,13 +17,29 @@ nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 """
 
 
+async def search_for_events():
+    await asyncio.sleep(2)
+    print("searching for events...")
+
+
+async def analize_data():
+    await asyncio.sleep(1)
+    print("analyzing data...")
+
+
+async def generate_response():
+    await asyncio.sleep(1)
+    print("generating response...")
+    return "".join([str(random.randint(1, 9)) for i in range(10)])
+
+
 def stream_response(response: str):
     for word in response.split():
         yield word + " "
         time.sleep(0.02)
 
 
-def display_conversation():
+async def display_conversation():
     for user_prompt, bot_response in zip(user_prompts[:-1], bot_responses[:-1]):
         st.chat_message("user").write(user_prompt)
         st.chat_message("assistant").write(bot_response)
@@ -32,13 +49,14 @@ def display_conversation():
         with empty_space.container():
             with st.status("Generating response...", expanded=True) as status:
                 st.write("Searching for events...")
-                time.sleep(2)
+                await search_for_events()
                 st.write("Analyzing data...")
-                time.sleep(1)
+                await analize_data()
                 st.write("Constructing final message...")
-                time.sleep(1)
+                response = await generate_response()
                 status.update(label="Complete!", state="complete", expanded=False)
-                time.sleep(1)
+                time.sleep(0.5)
+                bot_responses.append(response)
         empty_space.empty()
         st.write_stream(stream_response(bot_responses[-1]))
 
@@ -46,6 +64,5 @@ def display_conversation():
 user_prompt = st.chat_input("Ask a question about tech meetups in Poland")
 if user_prompt:
     user_prompts.append(user_prompt)
-    bot_responses.append("".join([str(random.randint(1, 9)) for i in range(10)]))
     print({"user_prompts": user_prompts, "bot_responses": bot_responses})
-    display_conversation()
+    asyncio.run(display_conversation())
