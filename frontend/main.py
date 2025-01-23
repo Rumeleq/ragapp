863edc179b03a6039f858d7dab27c0e2b03e270e
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-23 13:09:47 +0100

    add lorem ipsum as a mock response

--- frontend/main.py
+++ frontend/main.py
@@ -3,4 +3,10 @@ import streamlit as st
 st.title("RAG app")
 st.write("Welcome in the conversation with chatbot, that will tell you about tech meetups in Poland!")
 
+LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
 user_prompt = st.text_input("You: ", "")
